cff3fcafef1e8efc5cf5dd6560112ca4b72ed858
rvdvivedi <rohan.dvivedi@oyorooms.com>
2018-08-25 21:02:55 +0530

    server and basic serve functions are ready

--- src/serve.c
+++ src/serve.c
@@ -1,13 +1,34 @@
 #include<string.h>
 #include<sys/socket.h>
-
 #include<stdio.h>
 
+// once the connection is established this function is called by base Server to accept the request
+// handle parsing and all the other stuff till the response objectb is sent to the controller
 void serve(int fd)
 {
-	char ms[2000];
-	int n = recv(fd, ms, 1500, 0);
-	printf("%.*s %d\n",n,ms,n);
-	int chars = send(fd , message , strlen(message) , 0);
-	printf("%d chars sent\n",chars);
+	char buffer[100];
+	int buffreadlength;
+	do
+	{
+		bufferreadlength = recv();
+		parseAndStoreStatedRequest(buffer,100);
+	}
+	while( bufferreadlength == 100 );
+
+	// retrieve request httpobject generated from parse and store function
+	// pass request to a method that deploys to various controllers
+	// retrieve response in httpobject from the controller
+
+	response = encodeToHttpResponse();
+	send(fd,response,strlen(response),0);
+}
+
+void parseAndStoreStatedRequest(char* requestString,int length)
+{
+
+}
+
+char* encodeToHttpResponse(char* )
+{
+
 }
