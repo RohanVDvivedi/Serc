7ead0a57ef1939175b23f717f9db5a6a7ca09286
rvdvivedi <rohan.dvivedi@oyorooms.com>
2018-08-29 15:54:06 +0530

    route.py is well commented as of now ans is making good enough distributer.c

--- pyt/route.py
+++ pyt/route.py
@@ -26,13 +26,16 @@ def getHashValue(s):
 	return len(s)
 """
 
+# read the file that contains route configurations
+routing_config_file = open("./con/routing.con","r")
 
-routing_config_file = open("../con/routing.con","r")
-
+# since the file is in json use json.loads to use it as onject
 routes = json.loads(routing_config_file.read())
 
 routing_config_file.close()
 
+# use a dictionery to store the routes in format as required for making switch case
+# format is mydict[method][hashvalue_of_path][path_in_string] = controller
 mydict = {}
 for route in routes :
 	for method in route['methods']:
@@ -45,39 +48,50 @@ for route in routes :
 			mydict[method][hashval][path] = route['controller']
 
 
-
-case_string          = "\tswitch(METHOD)"
-case_string         += "\n\t{"
+"""
+outer most routing part is switch by HttpMethodType
+after that we route by the hash value of path
+but note that any 2 strings can and may have same hash value
+so for this we compare all the strings having the same hash value
+"""
+# create whole of case statement in case_string
+case_string              = "\tswitch(METHOD)"
+case_string             += "\n\t{"
 for method in mydict:
-	case_string     += "\n\t\tcase " + method + " :"
-	case_string     += "\n\t\t{"
-	case_string     += "\n\t\t\tswitch(PATH)"
-	case_string     += "\n\t\t\t{"
+	case_string         += "\n\t\tcase " + method + " :"
+	case_string         += "\n\t\t{"
+	case_string         += "\n\t\t\tswitch(PATH)"
+	case_string         += "\n\t\t\t{"
 	for hashval in mydict[method]:
-		case_string += "\n\t\t\t\tcase " + str(hashval) + " :"
-		case_string += "\n\t\t\t\t{"
+		case_string     += "\n\t\t\t\tcase " + str(hashval) + " :"
+		case_string     += "\n\t\t\t\t{"
 		for path in mydict[method][hashval]:
-			case_string += "\n\t\t\t\t\t// case for path = " + path
-			case_string += "\n\t\t\t\t\tif( strcmp( path_str , \"" + path + "\" ) )"
+			case_string += "\n\t\t\t\t\t// case for path = " + path + " and supports method = " + method
+			case_string += "\n\t\t\t\t\tif( 0 == strcmp( path_str , \"" + path + "\" ) )"
 			case_string += "\n\t\t\t\t\t{"
 			case_string += "\n\t\t\t\t\t\terror = " + mydict[method][hashval][path] + "(hrq,hrp);"
 			case_string += "\n\t\t\t\t\t\trouting_resolved = 1;"
 			case_string += "\n\t\t\t\t\t}"
-		case_string += "\n\t\t\t\t\tbreak;"
-		case_string += "\n\t\t\t\t}"
-	case_string     += "\n\t\t\t}"
-	case_string     += "\n\t\t\tbreak;"
-	case_string     += "\n\t\t}"
-case_string += "\n\t}\n"
+		case_string     += "\n\t\t\t\t\tbreak;"
+		case_string     += "\n\t\t\t\t}"
+	case_string         += "\n\t\t\t}"
+	case_string         += "\n\t\t\tbreak;"
+	case_string         += "\n\t\t}"
+case_string             += "\n\t}\n"
 		
 #print (case_string)
 
 
 
 
-Templatefile = open("./distributer.temp","r");
-codefile = open("./distributer.c","w");
 
+
+
+
+Templatefile = open("./pyt/distributer.temp","r");
+codefile = open("./src/distributer.c","w");
+
+#replace case string for the comment "//@switchcase\n" in c fil
 for line in Templatefile:
 	if line == "//@switchcase\n":
 		codefile.write(case_string)
