b8d50f1e17b7113aa4be89290f5104c6827a30dc
Rohan Dvivedi <rohandvivedi@gmail.com>
2018-08-29 13:36:52 +0530

    case statement maker completed as a python script

--- pyt/route.py
+++ pyt/route.py
@@ -1,6 +1,7 @@
 import json;
 
 # same as getHashValue function in c in strhsh
+"""
 def getHashValue(s):
 	ans = 0;i = 1;last = 0;curr = 0;diff = 0;lastoccur = [0] * 128;iter_i = 0
 	while iter_i < len(s) :
@@ -18,7 +19,10 @@ def getHashValue(s):
 		if i%5 == 0 :
 			ans = ans + ((ans%13)*(diff+2)*(diff%3)*(int(diff/7))) + ((ans%29)*last) + ((ans%37)*curr) + (ans%11)
 	return ans
+"""
 
+def getHashValue(s):
+	return len(s)
 
 
 routing_config_file = open("../con/routing.con","r")
@@ -34,7 +38,7 @@ for route in routes :
 			hashval = getHashValue(path)
 			if not (hashval in mydict[method]):
 				mydict[method][hashval] = {}
-			mydict[method][hashval][path] = controller
+			mydict[method][hashval][path] = route['controller']
 
 
 
@@ -49,15 +53,15 @@ for method in mydict:
 		case_string += "\n\t\t\t\t// case for path = "
 		case_string += "\n\t\t\t\tcase " + str(hashval) + " :"
 		case_string += "\n\t\t\t\t{"
-		case_string += "\n\t\t\t\t\tif( strcmp( path_str , \"" + path + "\" ) )"
-		case_string += "\n\t\t\t\t\t{"
-		case_string += "\n\t\t\t\t\t\tint error = " + mydict[method][path] + "(hrq,hrp);"
-		case_string += "\n\t\t\t\t\t\tint routing_resolved = 1;"
-		case_string += "\n\t\t\t\t\t{"
+		for path in mydict[method][hashval]:
+			case_string += "\n\t\t\t\t\tif( strcmp( path_str , \"" + path + "\" ) )"
+			case_string += "\n\t\t\t\t\t{"
+			case_string += "\n\t\t\t\t\t\tint error = " + mydict[method][hashval][path] + "(hrq,hrp);"
+			case_string += "\n\t\t\t\t\t\tint routing_resolved = 1;"
+			case_string += "\n\t\t\t\t\t}"
 		case_string += "\n\t\t\t\t}"
 	case_string     += "\n\t\t\t}"
 	case_string     += "\n\t\t}"
-
 case_string += "\n\t}\n"
 		
 print (case_string)
