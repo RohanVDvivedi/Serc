84e20812dfaf68e1880c9dbd5f57e151f58f1e19
Rohan Dvivedi <rohandvivedi@gmail.com>
2018-08-30 10:45:37 +0530

    listed controller functions in route.py and further organized

---
+++ src/distributer.c
@@ -0,0 +1,64 @@
+#include<distributer.h>
+
+void distribute(HttpRequest* hrq,HttpResponse* hrp)
+{
+	printHttpRequest(hrq);
+
+	char* path_str   = hrq->Path;
+	unsigned long long int PATH   = getHashValue(hrq->Path);
+	HttpMethodType METHOD = hrq->MethodType;
+
+	int routing_resolved = 0;
+	int error = 0;
+
+	switch(METHOD)
+	{
+		case GET :
+		{
+			switch(PATH)
+			{
+				case 143199 :
+				{
+					// case for path = /index and supports method = GET
+					if( 0 == strcmp( path_str , "/index" ) )
+					{
+						error = first_controller(hrq,hrp);
+						routing_resolved = 1;
+					}
+					break;
+				}
+			}
+			break;
+		}
+		case POST :
+		{
+			switch(PATH)
+			{
+				case 143199 :
+				{
+					// case for path = /index and supports method = POST
+					if( 0 == strcmp( path_str , "/index" ) )
+					{
+						error = first_controller(hrq,hrp);
+						routing_resolved = 1;
+					}
+					break;
+				}
+			}
+			break;
+		}
+	}
+
+	if(routing_resolved==0)
+	{
+		hrp->Status = 404;
+	}
+
+	setServerDefaultHeaderInResponse(hrp);
+	if(hrp->ResponseBody == NULL)
+	{
+		hrp->ResponseBodyLength = 0;
+	}
+
+	printHttpResponse(hrp);
+}
