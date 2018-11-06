cf4ea39736dcc3cc2b1951de306ba3fa0a4acf9e
Rohan Dvivedi <rohandvivedi@gmail.com>
2018-11-07 01:56:59 +0530

    array.h has been debugged and tested vigorously

--- tempmain.c
+++ tempmain.c
@@ -1,21 +1,88 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<MyObject_json.h>
+
+#define object int
+#include<array.h>
+
+void print(array_int* a)
+{
+	printf("\n\nsize = %llu, max_size = %llu\n",a->size,a->max_size);
+	for(int i=0;i<a->size;i++)
+	{
+		if( get_int(a,i)!=NULL )
+		{
+			printf("%d -> %d\n",i,(*(get_int(a,i))));
+		}
+	}
+}
 
 int main()
 {
-	MyObject obj;
-	obj.myint = -100;
-	obj.mylongint = -200;
-	obj.myuint = 100;
-	obj.myulongint = 500;
-	obj.mystring = "wow task base done";
+	array_int* a = getArray_int();
+	int temp;
 
-	char* json = MyObject_toJson( &obj );
+	temp = 1;
+	add_int(a,&temp);
 
-	printf("\n\n--%s--\n\n",json);
+	temp = 2;
+	add_int(a,&temp);
+	print(a);
 
-	free(json);
+	temp = 3;
+	add_int(a,&temp);
 
+	temp = 4;
+	add_int(a,&temp);
+
+	temp = 5;
+	add_int(a,&temp);
+
+	temp = -6;
+	add_int(a,&temp);
+
+	temp = -7;
+	add_int(a,&temp);
+
+	temp = -8;
+	add_int(a,&temp);
+
+	temp = 1;
+	add_int(a,&temp);
+
+	temp = 2;
+	add_int(a,&temp);
+
+	temp = 3;
+	add_int(a,&temp);
+
+	temp = 4;
+	add_int(a,&temp);
+
+	temp = -1;
+	add_int(a,&temp);
+
+	temp = -2;
+	add_int(a,&temp);
+
+	temp = -3;
+	add_int(a,&temp);
+
+	temp = 0;
+	add_int(a,&temp);
+
+	print(a);
+
+	temp = -6;
+	remove_int(a,&temp);
+
+	remove_int(a,get_int(a,5));
+
+	print(a);
+
+	temp = -6;
+	int temp1 = 5;
+	printf("-6 exists = %d\n 5 exists = %d\n",exists_int(a,&temp),exists_int(a,&temp1));
+
+	deleteArray_int(a);
 	return 0;
 }
