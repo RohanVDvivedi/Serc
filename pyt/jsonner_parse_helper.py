559e4d5987c865eaa0f63182fb5277d7e5a11481
Rohan Dvivedi <rohandvivedi@gmail.com>
2018-11-08 14:49:24 +0530

    added provision to handle pointer variables in  jsonner

--- pyt/jsonner_parse_helper.py
+++ pyt/jsonner_parse_helper.py
@@ -100,7 +100,7 @@ def identify_data_type(datatype_string) :
 	return DataType.OTHER
 
 def identify_data_type_flexible(datatype_string) :
-	for key,list_data_type_string in datatype_handler.dataTypeStrings_.items() :
+	for key,list_data_type_string in datatype_handler.dataTypeStrings_flexible.items() :
 		for data_type_string in list_data_type_string :
 			if data_type_string in datatype_string :
 				return key
