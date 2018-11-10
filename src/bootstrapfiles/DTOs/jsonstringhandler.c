81a8a592348926bebd91ab503360ff2f73050fd3
Rohan Dvivedi <rohandvivedi@gmail.com>
2018-11-11 00:36:44 +0530

    added required template files and object_json_handler.py script for handleing the addition and deletion of the code for various case handling in object_json, and updates jsonstringhanfdler.c to handle cases for NULL inoput parameters

--- src/bootstrapfiles/DTOs/jsonstringhandler.c
+++ src/bootstrapfiles/DTOs/jsonstringhandler.c
@@ -3,15 +3,27 @@
 JsonString* getJsonString(char* str)
 {
 	JsonString* JS = (JsonString*)malloc(sizeof(JsonString));
-	JS->size = strlen(str) + 1;
+	int str_length = (str==NULL ? 0 : strlen(str)) + 1;
+	JS->size = str_length + 1;
 	JS->max_size = (JS->size * 4) + 4;
 	JS->string = (char*)malloc(JS->max_size);
-	strcpy(JS->string,str);
+	if(str != NULL)
+	{
+		strcpy(JS->string,str);
+	}
+	else
+	{
+		JS->string[0] = '\0';
+	}
 	return JS;
 }
 
 void addToJsonString(JsonString* JS1,char* JS2)
 {
+	if(JS2 == NULL || JS1 == NULL || JS1->string == NULL)
+	{
+		return;
+	}
 	int JS2size = strlen(JS2) + 1;
 	int new_size = JS1->size + JS2size - 1;
 	if(new_size <= JS1->max_size)
@@ -34,6 +46,10 @@ void addToJsonString(JsonString* JS1,char* JS2)
 
 void deleteJsonString(JsonString* JS)
 {
+	if(JS == NULL)
+	{
+		return;
+	}
 	if(JS->max_size > 0 && JS->string!=NULL)
 	{
 		free(JS->string);
