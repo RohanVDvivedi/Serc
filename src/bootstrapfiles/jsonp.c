05841caaf0bc657587075ee2039bea9e2a943c5a
Rohan Dvivedi <rohandvivedi@gmail.com>
2018-12-16 11:51:50 +0530

    initial commit to json parser to read json data from request and response bodies

---
+++ src/bootstrapfiles/jsonp.c
@@ -0,0 +1,18 @@
+#include<jsonp.h>
+
+json_error json_parse(char* json,json_error* error)
+{
+	json_node* node;
+
+	return node;
+}
+
+void json_print(json_node* node)
+{
+	
+}
+
+void json_delete(json_node* node)
+{
+
+}
