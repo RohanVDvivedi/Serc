2e46677d110c00f396d2f6f1a950dbbf4b9a970e
Rohan Dvivedi <rohandvivedi@gmail.com>
2018-12-16 11:03:52 +0530

    removed code that doesn't need testing from tempmain

--- tempmain.c
+++ tempmain.c
@@ -9,57 +9,8 @@ extern FILE* ServerLog;
 extern char* tag;
 
 int main()
-{/*
-	MyObject* m = get_MyObject();
-
-	m->myint = 1;
-	m->mylongint = 2;
-	m->myuint = 3;
-	m->myulongint = 4;
-	m->mystring = "hello";
-	m->my_bool = 0;
-	m->myfloat = 6.9987;
-	m->mydouble = 5.9876577;
-
-	long long int i = 1001;
-	double dou = 10.101;
-	unsigned char bool_= 0;
-	char* arr_str = "world";
-
-	add(m->my_array,&i,INTEGER_JSON,sizeof(long long int));
-	add(m->my_array,&dou,DOUBLE_JSON,sizeof(double));
-	add(m->my_array,&bool_,BOOLEAN_JSON,sizeof(unsigned char));
-	add(m->my_array,NULL,NULL_JSON,0);
-	add(m->my_array,arr_str,STRING_JSON,strlen(arr_str)+1);
-	add(m->my_array,NULL,ARRAY_JSON,0);
-	add(m->my_array,NULL,MYOBJECTSUB_JSON,0);
-
-	((MyObjectSub*)(get(m->my_array,6)->Data))->a = 69;
-	((MyObjectSub*)(get(m->my_array,6)->Data))->b = "hello sexy";
-
-	array_json* arr_temp = get(m->my_array,5)->Data;
-
-	i = 10;
-	dou = 0.101;
-	bool_= 1;
-	arr_str = "India to canada";
-
-	add(arr_temp,&i,INTEGER_JSON,sizeof(long long int));
-	add(arr_temp,&dou,DOUBLE_JSON,sizeof(double));
-	add(arr_temp,&bool_,BOOLEAN_JSON,sizeof(unsigned char));
-	add(arr_temp,NULL,NULL_JSON,0);
-	add(arr_temp,arr_str,STRING_JSON,strlen(arr_str)+1);
-
-	m->my_sub->a = 500;
-	m->my_sub->b = "world";
-
-	char* c = MyObject_toJson(m);
-	printf("\n==%s==\n",c);
-	free(c);
-
-	delete_MyObject(m);
-	return 0;*/
-	// get pointer to log file
+{
+/*	// get pointer to log file
 	ServerLog = createLogFile("ServerLog");
 
 	char* host = "localhost";
@@ -82,4 +33,7 @@ int main()
 	setServerDefaultHeaderInRequest(hr);
 	retrieveResponse(host,port,hr,NULL);
 	deleteHttpRequest(hr);
+	return 0;*/
+
+	
 }
