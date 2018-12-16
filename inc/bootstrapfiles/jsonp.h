da906c46f5e87f74eba11bbf670a868e66fc5da3
Rohan Dvivedi <rohandvivedi@gmail.com>
2018-12-16 13:17:59 +0530

    decided logic and json node stree structure

--- inc/bootstrapfiles/jsonp.h
+++ inc/bootstrapfiles/jsonp.h
@@ -5,10 +5,48 @@
 #include<stdlib.h>
 #include<string.h>
 
+typedef enum json_node_type json_node_type;
+enum json_node_type
+{
+	STRING_JSON,
+	NUMBER_JSON,
+	NULL_JSON,
+	TRUE_JSON,
+	FALSE_JSON
+	ARRAY_JSON,
+	OBJECT_JSON
+};
+
+/*
+	every node contents will start and end with an index
+	object start and end index will always be at characters { and }
+	object will always be consisting of key:value pair but not values directly
+	array start and end index will always be at characters [ and ]
+	none of array element can be a key:value pair
+	note : key can only be a string token
+	if child_count is 1, child will get popinted to by child pointer
+	if child_count is more than one in case of object or array
+	the children will be in children array each of which will point to each child individually
+	type will specify what each of node is
+	example
+	: {"x":"y","z":3,"w":[{"q":true},2]}
+	{}->S"x"->S"y"
+		S"z"->N3
+		S"w"->[]->{}->Sq->true
+		        ->N2
+*/
+
 typedef struct json_node json_node;
 struct json_node
 {
-
+	json_node** children;
+	json_node* child;
+	json_node* parent;
+	char* start_index;
+	char* end_index;
+	int child_count;
+	json_node_type type;
+	int is_key;
 };
 
 typedef enum json_error json_error;
@@ -16,16 +54,6 @@ enum json_error
 {
 	NO_ERROR = 0,
 	UNKNOWN_ERROR = -1
-};
-
-typedef enum json_token_type json_token_type;
-enum json_token_type
-{
-	STRING_JSON,
-	NUMBER_JSON,
-	NULL_JSON,
-	TRUE_JSON,
-	FALSE_JSON
 };
 
 json_node* json_parse(char* json,json_error* error);
