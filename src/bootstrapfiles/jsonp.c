e0ba0ab1f4f252528754a6c420eca4f2af14909d
Rohan Dvivedi <rohandvivedi@gmail.com>
2018-12-21 01:39:31 +0530

    completed json p add child method

--- src/bootstrapfiles/jsonp.c
+++ src/bootstrapfiles/jsonp.c
@@ -20,7 +20,9 @@ int add_child(json_node* parent,json_node* child);
 
 json_node* json_parse(char* json,json_error* error)
 {
-	json_node* node;
+	json_node* node = NULL;
+
+
 
 	return node;
 }
@@ -77,24 +79,45 @@ void json_print(json_node* node,int n_spaces)
 	}
 }
 
+void remove_child(json_node* node)
+{
+	if(node->child != NULL)
+	{
+		json_delete(node->child);
+	}
+	node->child = NULL;
+}
+
+void remove_children(json_node* node)
+{
+	if(node->children != NULL)
+	{
+		for(int i=0;i<node->child_count;i++)
+		{
+			if(node->children[i] != NULL)
+			{
+				json_delete(node->children[i]);
+			}
+			node->children[i] = NULL;
+		}
+		node->child_count = 0;
+		if( node->child_container_size > 0 && node->children != NULL)
+		{
+			free(node->children);
+		}
+		node->child_container_size = 0;
+		node->children = NULL;
+	}
+}
+
 void json_delete(json_node* node)
 {
 	if(node == NULL)
 	{
 		return;
 	}
-	if(node->child != NULL)
-	{
-		json_delete(node->child);
-	}
-	if(node->children != NULL)
-	{
-		for(int i=0;i<node->child_count;i++)
-		{
-			json_delete(node->children[i]);
-		}
-		free(node->children);
-	}
+	remove_child(node);
+	remove_children(node);
 	free(node);
 	return;
 }
@@ -106,13 +129,34 @@ int add_child(json_node* parent,json_node* child)
 	{
 		return -1;
 	}
-	else if( parent->type == ARRAY_JSON || parent->type == OBJECT_JSON )
+	else if( ( parent->type == ARRAY_JSON && child->is_key == 0 ) || ( parent->type == OBJECT_JSON && child->is_key == 1 ) )
 	{
-
+		remove_child(node);
+		if(node->child_container_size <= node->child_count || node->child_container_size == 0)
+		{
+			int new_child_container_size = 2 * node->child_container_size + 3;
+			json_node** new_children = (json_node**) calloc(new_child_container_size,sizeof(json_node*));
+			for(int i=0;i<node->child_count;i++)
+			{
+				new_children[i] = node->children[i];
+			}
+			if(node->children != NULL)
+			{
+				free(node->children);
+			}
+			node->children = new_children;
+			node->child_container_size = new_child_container_size;
+		}
+		if(node->child_container_size > node->child_count)
+		{
+			node->children[node->child_count++] = child;
+		}
 	}
-	else if(parent->is_key == 1)
+	else if(parent->is_key == 1 && parent->type == STRING_JSON)
 	{
-
+		remove_child(node);
+		remove_children(node);
+		node->child = child;
 	}
 	else
 	{
