87f66317e97d09ea9d853c821a062f9c2944eca6
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-01-13 00:29:19 +0530

    automatic building methods for combined.c added functionality

--- src/bootstrapfiles/DTOs/combined.c
+++ src/bootstrapfiles/DTOs/combined.c
@@ -37,3 +37,115 @@ char* toJson(void* data,Type_Support type)
 	}
 	return return_string;
 }
+
+void* fromJson(char* json_str,Type_Support type)
+{
+	void* return_data = NULL;
+	if(data!=NULL)
+	{
+		switch(type)
+		{
+			case ARRAY_JSON :
+			{
+				return_data = (void*) array_json_fromJson(json_str);
+				break;
+			}
+			/*
+			case CLASSNAME_JSON :
+			{
+				return_data = (void*) classname_fromJson(json_str);
+				break;
+			}
+			*/
+			case MYOBJECT_JSON :
+			{
+				return_data = (void*) MyObject_fromJson(json_str);
+				break;
+			}
+			case MYOBJECTSUB_JSON :
+			{
+				return_data = (void*) MyObjectSub_fromJson(json_str);
+				break;
+			}
+			default :
+			{
+				break;
+			}
+		}
+	}
+	return return_data;
+}
+
+void* get(Type_Support type)
+{
+	void* return_data = NULL;
+	if(data!=NULL)
+	{
+		switch(type)
+		{
+			case ARRAY_JSON :
+			{
+				return_data = (void*) get_array((array_json*)data);
+				break;
+			}
+			/*
+			case CLASSNAME_JSON :
+			{
+				return_string = (void*) get_classname();
+				break;
+			}
+			*/
+			case MYOBJECT_JSON :
+			{
+				return_data = (void*) get_MyObject();
+				break;
+			}
+			case MYOBJECTSUB_JSON :
+			{
+				return_data = (void*) get_MyObjectSub();
+				break;
+			}
+			default :
+			{
+				break;
+			}
+		}
+	}
+	return return_data;
+}
+
+void del(void* data,Type_Support type)
+{
+	if(data!=NULL)
+	{
+		switch(type)
+		{
+			case ARRAY_JSON :
+			{
+				delete_array((array_json*)data);
+				break;
+			}
+			/*
+			case CLASSNAME_JSON :
+			{
+				delete_classname((classname*)data);
+				break;
+			}
+			*/
+			case MYOBJECT_JSON :
+			{
+				delete_MyObject((MyObject*)data);
+				break;
+			}
+			case MYOBJECTSUB_JSON :
+			{
+				delete_MyObjectSub((MyObjectSub*)data);
+				break;
+			}
+			default :
+			{
+				break;
+			}
+		}
+	}
+}
