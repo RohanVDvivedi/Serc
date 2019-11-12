76564ab626832563cf3f4ceb9b53ba45eca9ffc6
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-11-12 20:10:48 +0530

    added a controller to resolve file paths

--- inc/bootstrapfiles/file_request_resolution_controller.h
+++ inc/bootstrapfiles/file_request_resolution_controller.h
@@ -1,7 +1,12 @@
 #ifndef FILE_REQUEST_RESOLUTION_CONTROLLER_H
 #define FILE_REQUEST_RESOLUTION_CONTROLLER_H
 
+#include<stdio.h>
+#include<unistd.h>
+
 #include<httpobject.h>
+
+#define SERC_ROOT_PATH "./serc_root"
 
 int file_request_controller(HttpRequest* hrq, HttpResponse* hrp, int* routing_resolved);
 
--- src/bootstrapfiles/file_request_resolution_controller.c
+++ src/bootstrapfiles/file_request_resolution_controller.c
@@ -2,5 +2,33 @@
 
 int file_request_controller(HttpRequest* hrq, HttpResponse* hrp, int* routing_resolved)
 {
+	if(hrq->method == GET && hrq->path->cstring[0] == '/')
+	{
+		dstring* file_path = get_dstring(SERC_ROOT_PATH, 10);
+		concatenate_dstring(file_path, hrq->path);
+
+		if(access(file_path->cstring, R_OK) != -1) {
+    		FILE* file = fopen(file_path->cstring, "rb");
+
+    		#define FILE_READ_BUFFER_SIZE 100
+    		char file_buffer[FILE_READ_BUFFER_SIZE];
+
+    		while(1)
+    		{
+    			int read_count = fread(file_buffer, FILE_READ_BUFFER_SIZE - 1, sizeof(char), file);
+    			file_buffer[read_count] = '\0';
+    			if(read_count >= 0)
+    			{
+    				append_to_dstring(hrp->body, file_buffer);
+    			}
+    			else
+    			{
+    				break;
+    			}
+			}
+
+    		fclose(file);
+		}
+	}
 	return 0;
 }
