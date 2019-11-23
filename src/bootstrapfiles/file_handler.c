693c6e0ec606e17d40e61e9d79bfc4691589aac5
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-11-24 01:55:13 +0530

    made the file_cache thread safe using the reader writer lock

--- src/bootstrapfiles/file_handler.c
+++ src/bootstrapfiles/file_handler.c
@@ -3,12 +3,21 @@
 // this is where we will cache the file that we will serve,
 // we read from the server root, to send in response will be sent 
 hashmap* file_cache = NULL;
+rwlock* file_cache_rwlock = NULL;
+
+typedef struct file_cache_component file_cache_component;
+struct file_cache_component
+{
+	dstring* file_content;
+	rwlock* file_content_rwlock;
+};
 
 void init_file_cache_if_empty()
 {
 	if(file_cache == NULL)
 	{
 		file_cache = get_hashmap(30, (unsigned long long int (*)(const void*))getHashValueDstring, (int (*)(const void*, const void*))compare_dstring, ELEMENTS_AS_RED_BLACK_BST);
+		file_cache_rwlock = get_rwlock();
 	}
 }
 
@@ -16,15 +25,23 @@ int read_file_in_dstring(dstring* file_contents_result, dstring* file_path)
 {
 	init_file_cache_if_empty();
 
-	// find the file from the cache, once the file has been requested from the server it gets cached, in page cache 
-	dstring* file_content_from_cache = (dstring*)(find_value_from_hash(file_cache, file_path));
+	// find the file from the cache, once the file has been requested from the server it gets cached, in page cache
+	read_lock(file_cache_rwlock);
+	file_cache_component* file_content_from_cache = (file_cache_component*)(find_value_from_hash(file_cache, file_path));
+	read_unlock(file_cache_rwlock);
 
 	// if we cache miss, we need to go to disk, we go to disk only if we confirm that the file is present
 	if(file_content_from_cache == NULL && access(file_path->cstring, R_OK) != -1)
 	{
 		// insert the cache entry, so we do not miss the cache next time
-		file_content_from_cache = get_dstring("", 10);
+		file_content_from_cache = (file_cache_component*)malloc(sizeof(file_cache_component));
+		file_content_from_cache->file_content = get_dstring("", 10);
+		file_content_from_cache->file_content_rwlock = get_rwlock();
+
+		// write newly built component to cache
+		write_lock(file_cache_rwlock);
 		insert_entry_in_hash(file_cache, get_dstring(file_path->cstring ,10), file_content_from_cache);
+		write_unlock(file_cache_rwlock);
     	
     	// open the file
     	FILE* file = fopen(file_path->cstring, "rb");
@@ -38,7 +55,9 @@ int read_file_in_dstring(dstring* file_contents_result, dstring* file_path)
 			long long int read_count = fread(file_buffer, sizeof(char), FILE_READ_BUFFER_SIZE, file);
 			if(read_count >= 0)
 			{
-				appendn_to_dstring(file_content_from_cache, file_buffer, read_count);
+				write_lock(file_content_from_cache->file_content_rwlock);
+				appendn_to_dstring(file_content_from_cache->file_content, file_buffer, read_count);
+				write_unlock(file_content_from_cache->file_content_rwlock);
 			}
 		}
 
@@ -48,7 +67,9 @@ int read_file_in_dstring(dstring* file_contents_result, dstring* file_path)
 	// if cache had been updated, we read from the cache variable
 	if(file_content_from_cache != NULL)
 	{
-		concatenate_dstring(file_contents_result, file_content_from_cache);
+		read_lock(file_content_from_cache->file_content_rwlock);
+		concatenate_dstring(file_contents_result, file_content_from_cache->file_content);
+		read_unlock(file_content_from_cache->file_content_rwlock);
 		return 0;
 	}
 	return -1;
