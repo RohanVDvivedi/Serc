6c284470e04da43112f3cb1f728e2efab0b03d8e
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-01-17 23:48:11 +0100

    minor fix for making the add/remove header, separested server and client test files fo they do not afftect each other

---
+++ client.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<http_client.h>
+
+int main()
+{
+	transaction_client* http_client = get_http_client("rohandvivedi.com", 1);
+
+	if(http_client == NULL)
+	{
+		printf("Could not find server or setup connection\n");
+		return 0;
+	}
+
+	HttpRequest* hrq = getNewHttpRequest();
+	hrq->method = GET;
+	append_to_dstring(hrq->path, "/api");
+	addHeader("Host", "rohandvivedi.com", hrq->headers);
+
+	job* promise = send_request_async(http_client, hrq);
+
+	HttpResponse* hrp = wait_or_get_response(promise, NULL);
+
+	if(hrp == NULL)
+	{
+		printf("Response : NULL\n\n");
+	}
+	else
+	{
+		printResponse(hrp);
+		deleteHttpResponse(hrp);
+	}
+
+	deleteHttpRequest(hrq);
+
+	shutdown_and_delete_http_client(http_client);
+	return 0;
+}
