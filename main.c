7307b36575c20d7881e7070faaf91a44c93d0acd
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-01-17 20:30:30 +0100

    setup of sample code to test http client

--- main.c
+++ main.c
@@ -1,10 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include<http_server.h>
+//#include<http_server.h>
+#include<http_client.h>
 
 int main()
 {
-	http_server_run(6900);
+	//http_server_run(6900);
+	{
+		transaction_client http_client = get_http_client("rohandvivedi.com", 1);
+
+		HttpRequest* hrq = getNewHttpRequest();
+
+		append_to_dstring(hrp->path, "/path");
+
+		job* promise = send_request_async(http_client, hrq);
+
+		HttpResponse* hrp = wait_or_get_response(promise, NULL);
+
+		if(hrp == NULL)
+		{
+			printf("Response : NULL\n\n");
+		}
+		else
+		{
+			printResponse(hrp);
+			deleteHttpResponse(hrp);
+		}
+
+		deleteHttpRequest(hrq);
+	}
 	return 0;
 }
