34bf58358fb026fdb5aaf83d90e8b532546c161e
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-06-26 21:56:19 +0530

    new https server run function is required for ssl-ed connection

--- inc/http_server.h
+++ inc/http_server.h
@@ -17,5 +17,6 @@
 // if you want to start multiple servers, in the same application
 // you must start all of them in the same thread
 void http_server_run(uint16_t PORT);
+void https_server_run(uint16_t PORT, char* ssl_cert_path);
 
 #endif
--- src/http_server.c
+++ src/http_server.c
@@ -127,7 +127,21 @@ void http_server_run(uint16_t PORT)
 	make_global_file_content_cache(fcc);
 
 	// start the server
-	connection_group cgp = get_connection_group_tcp_ipv4("127.0.0.1", 6900);
+	connection_group cgp = get_connection_group_tcp_ipv4("127.0.0.1", PORT);
+	serve(&cgp, http_connection_handler, 100, &listen_fd);
+
+	// delete the file cache
+	delete_file_content_cache(fcc);
+}
+
+void https_server_run(uint16_t PORT, char* ssl_cert_path)
+{
+	// initialize the content cache for serving the files
+	file_content_cache* fcc = get_file_content_cache();
+	make_global_file_content_cache(fcc);
+
+	// start the server
+	connection_group cgp = get_connection_group_tcp_ipv4("127.0.0.1", PORT);
 	serve(&cgp, http_connection_handler, 100, &listen_fd);
 
 	// delete the file cache
