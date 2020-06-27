f7e7acaca206862c8534baf0392710b8b1bd4468
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-06-27 11:22:55 +0530

    loading server certificates

--- con/config.h
+++ con/config.h
@@ -7,8 +7,8 @@
 // possible values are DEFLATE, GZIP, BROTLI
 
 // this defines what SSL encryption and SSL certificates folder would be used for serving content
-#define SSL_CERTIFICATES_PATH "./con"
-#define SSL_SERVER_METHOD	TLSv1_2_server_method 
+#define SSL_CERTIFICATION_AND_KEYS	"./con/cert_and_keys.pem"
+#define SSL_SERVER_METHOD			TLSv1_2_server_method 
 
 // for more options to SSL_SERVER_METHOD, use openSSL documentation
 
--- src/ssl_globals.c
+++ src/ssl_globals.c
@@ -2,6 +2,20 @@
 
 SSL_CTX* gbl_server_ssl_ctx = NULL;
 
+// for sample programs for building SSL applications use https://aticleworld.com/ssl-server-client-using-openssl-in-c/
+
+void load_certificates()
+{
+    if(SSL_CTX_use_certificate_file(gbl_server_ssl_ctx, SSL_CERTIFICATION_AND_KEYS, SSL_FILETYPE_PEM) <= 0)
+    	exit(-1);
+
+	if(SSL_CTX_use_PrivateKey_file(gbl_server_ssl_ctx, SSL_CERTIFICATION_AND_KEYS, SSL_FILETYPE_PEM) <= 0)
+    	exit(-1);
+
+	if(!SSL_CTX_check_private_key(gbl_server_ssl_ctx))
+    	exit(-1);
+}
+
 void init_gbl_server_ssl_ctx()
 {
 	SSL_library_init();
@@ -12,6 +26,8 @@ void init_gbl_server_ssl_ctx()
     	exit(-1);
 
     SSL_CTX_set_options(gbl_server_ssl_ctx, SSL_OP_CIPHER_SERVER_PREFERENCE);
+
+    load_certificates();
 }
 
 void deinit_gbl_server_ssl_ctx()
