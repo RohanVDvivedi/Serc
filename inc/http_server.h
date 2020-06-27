9649e3692fbc1591f22b0b02c97140fd574a2f8e
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-06-27 10:38:17 +0530

    init global server ssl context

--- inc/http_server.h
+++ inc/http_server.h
@@ -3,9 +3,12 @@
 
 #include<server.h>
 
+#include<ssl_globals.h>
+#include<http_https_connection_handler.h>
+
 // if you want to start multiple servers, in the same application
 // you must start all of them in the same thread
 void http_server_run(uint16_t PORT);
-void https_server_run(uint16_t PORT, char* ssl_cert_path);
+void https_server_run(uint16_t PORT);
 
 #endif
--- src/http_server.c
+++ src/http_server.c
@@ -1,5 +1,4 @@
 #include<http_server.h>
-#include<http_https_connection_handler.h>
 
 volatile int listen_fd = -1;
 void intHandler(int dummy)
@@ -24,15 +23,18 @@ void http_server_run(uint16_t PORT)
 	delete_file_content_cache(fcc);
 }
 
-void https_server_run(uint16_t PORT, char* ssl_cert_path)
+void https_server_run(uint16_t PORT)
 {
 	// initialize the content cache for serving the files
 	file_content_cache* fcc = get_file_content_cache();
 	make_global_file_content_cache(fcc);
 
 	// start the server using https connection handler
+	// you also need to create appropriate global ssl context, we leave that task to ssl_globals source
 	connection_group cgp = get_connection_group_tcp_ipv4("127.0.0.1", PORT);
+	init_gbl_server_ssl_ctx();
 	serve(&cgp, https_connection_handler, 100, &listen_fd);
+	deinit_gbl_server_ssl_ctx();
 
 	// delete the file cache
 	delete_file_content_cache(fcc);
