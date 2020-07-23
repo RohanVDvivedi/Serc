e476875dd18674fb387f0ecb0f8f1de2d07bf36a
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-23 16:53:59 +0530

    updated according to the api

--- server.c
+++ server.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#include<config.h>
+
 #include<http_server.h>
 
 int main()
 {
-	//http_server_run(80, 0);
-	http_server_run(443, 1);
+	http_server_run(80, SERC_ROOT_PATH, 0);
+	//http_server_run(443, SERC_ROOT_PATH, 1);
 	return 0;
 }
