4852dff89a3af1d652fb921d2925eeef6c6e132e
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-23 17:33:05 +0530

    added test connection callback functions

--- server.c
+++ server.c
@@ -5,9 +5,19 @@
 
 #include<http_server.h>
 
+void connection_started(int conn_fd, void* server_specific_params)
+{
+	printf("connection started\n");
+}
+
+void connection_finished(int conn_fd, void* server_specific_params)
+{
+	printf("connection finished\n");
+}
+
 int main()
 {
-	//http_server_run(80, SERC_ROOT_PATH, 0, NULL, NULL);
-	http_server_run(443, SERC_ROOT_PATH, 1, NULL, NULL);
+	//http_server_run(80, SERC_ROOT_PATH, 0, connection_started, connection_finished);
+	http_server_run(443, SERC_ROOT_PATH, 1, connection_started, connection_finished);
 	return 0;
 }
