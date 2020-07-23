a6488af087873169b05064b2227a4b5a819f30fc
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-23 17:07:15 +0530

    tested both https and http server

--- server.c
+++ server.c
@@ -7,7 +7,7 @@
 
 int main()
 {
-	http_server_run(80, SERC_ROOT_PATH, 0);
-	//http_server_run(443, SERC_ROOT_PATH, 1);
+	//http_server_run(80, SERC_ROOT_PATH, 0);
+	http_server_run(443, SERC_ROOT_PATH, 1);
 	return 0;
 }
