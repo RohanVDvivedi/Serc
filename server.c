7fa7676f060fb6007f1e39f4b8e8cb8a87f1dcbe
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-24 18:03:21 +0530

    the sample server updated to the new api

--- server.c
+++ server.c
@@ -5,19 +5,19 @@
 
 #include<http_server.h>
 
-void connection_started(int conn_fd, void* server_specific_params)
+void connection_started(int conn_fd, const void* params)
 {
-	printf("connection started\n");
+	printf("connection started %s\n", ((char*)params));
 }
 
-void connection_finished(int conn_fd, void* server_specific_params)
+void connection_finished(int conn_fd, const void* params)
 {
-	printf("connection finished\n");
+	printf("connection finished %s\n", ((char*)params));
 }
 
 int main()
 {
 	//http_server_run(80, SERC_ROOT_PATH, 0, connection_started, connection_finished);
-	http_server_run(443, /*NULL*/SERC_ROOT_PATH, 1, connection_started, connection_finished);
+	http_server_run(443, /*NULL*/SERC_ROOT_PATH, 1, connection_started, connection_finished, "-- Rohan");
 	return 0;
 }
