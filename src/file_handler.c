86fd58e6a2196630ba7c74b6ecfd446776e02f4d
rohan <rohandvivedi@gmail.com>
2021-12-23 23:50:11 +0530

    upgraded file handler.c to use updated cutlery dstring api

--- src/file_handler.c
+++ src/file_handler.c
@@ -28,21 +28,23 @@ int read_file_in_dstring(dstring* append_file_contents, file_cache* fc, dstring*
 	// cache miss case ---->>>
 
 	dstring file_path;
-	init_dstring(&file_path, NULL, 0);
+	init_empty_dstring(&file_path, 0);
 	concatenate_dstring(&file_path, &(fc->root_path));
 	concatenate_dstring(&file_path, relative_file_path);
-	expand_dstring(&file_path, file_path.bytes_occupied + 10);
-	file_path.cstring[file_path.bytes_occupied] = '\0';
+	sprint_chars(&file_path, '\0', 1);
+
+	// upon appending \0 file path is equivalent to a c string
+	const char* file_path_cstring = get_byte_array_dstring(&file_path);
 
 	// if the file is not in cache, check if the file even exists
 	// if the file does not exist, or if it is a folder in the server root, we return with -1
-	if(access(file_path.cstring, R_OK) != 0)
+	if(access(file_path_cstring, R_OK) != 0)
 	{
 		deinit_dstring(&(file_path));
 		return -1;
 	}
 	struct stat file_stat;
-	stat(file_path.cstring, &file_stat);
+	stat(file_path_cstring, &file_stat);
 	if(S_ISDIR(file_stat.st_mode))
 	{
 		deinit_dstring(&(file_path));
@@ -54,16 +56,16 @@ int read_file_in_dstring(dstring* append_file_contents, file_cache* fc, dstring*
     char file_buffer[FILE_READ_BUFFER_SIZE];
 
     // store the actual file contents which were requested in this dstring
-    dstring file_contents;	init_dstring(&file_contents, NULL, 0);
+    dstring file_contents;	init_empty_dstring(&file_contents, 0);
 
     // loop through the file, to put it in cache 
     // open the file
-    FILE* file = fopen(file_path.cstring, "rb");
+    FILE* file = fopen(file_path_cstring, "rb");
 		while(!feof(file))
 		{
 			long long int read_count = fread(file_buffer, sizeof(char), FILE_READ_BUFFER_SIZE, file);
 			if(read_count >= 0)
-				concatenate_dstring(&file_contents, dstring_DUMMY_DATA(file_buffer, read_count));
+				concatenate_dstring(&file_contents, &get_literal_dstring(file_buffer, read_count));
 		}
 	fclose(file);
 
@@ -88,8 +90,8 @@ void delete_file_cache(file_cache* fc)
 
 void get_extension_from_file_path(dstring* extension_result, dstring* path)
 {
-	char* path_t = path->cstring;
-	for(;path_t < path->cstring + path->bytes_occupied; path_t++)
+	char* path_t = get_byte_array_dstring(path);
+	for(;path_t < get_byte_array_dstring(path) + get_char_count_dstring(path); path_t++)
 	{
 		if((*path_t) == '.')
 			break;
@@ -97,6 +99,6 @@ void get_extension_from_file_path(dstring* extension_result, dstring* path)
 	if((*path_t) == '.')
 	{
 		path_t++;
-		concatenate_dstring(extension_result, dstring_DUMMY_DATA(path_t, path->cstring + path->bytes_occupied - path_t));
+		concatenate_dstring(extension_result, &get_literal_dstring(path_t, get_byte_array_dstring(path) + get_char_count_dstring(path) - path_t));
 	}
 }
