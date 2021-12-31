d450433657c71f19de695af030e8b90ac1bf06c7
rohan <rohandvivedi@gmail.com>
2021-12-31 15:45:01 +0530

    updated test client as per serc api

--- client.c
+++ client.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-	transaction_client* http_client = get_http_client("localhost", "80", 1);
+	transaction_client* http_client = new_http_client("localhost", "80", 1);
 
 	if(http_client == NULL)
 	{
@@ -14,9 +14,9 @@ int main()
 	}
 
 	HttpRequest hrq;
-	initHttpRequest(&hrq);
+	initHttpRequest(&hrq, -1);
 	hrq.method = GET;
-	concatenate_dstring(&(hrq.path), dstring_DUMMY_CSTRING("/api/project"));
+	concatenate_dstring(&(hrq.path), &get_literal_cstring("/api/project"));
 	insert_unique_in_dmap_cstr(&(hrq.parameters), "names", "DummyProject,ConnMan,Serc");
 
 	//append_to_dstring(&(hrq.body), "{\"Name\":\"project-name\",\"ProjectField\":\"the field : embedded, fpga, electronics, backend, frontend, system programming etc\",\"ProjectDescriptionShort\":\"project description short\",\"ProjectDescriptionLong\":\"project description at length, this will be shown on the inner when the user opens the project\",\"ProjectImage\":\"/img/pcb.jpeg\",\"GithubLink\":\"https://github.com/RohanVDvivedi/rohandvivedi.com\",\"YoutubeLink\":\"https://www.youtube.com/watch?v=fjIN9XPqp_A\",\"Explanation\":[{\"ExplanationText\":\"this must be a valid html consisting only of inline contents\",\"ExplanationImage\":\"link to the image that we show next to the given paragraph\"},{\"ExplanationText\":\"this must be a valid html consisting only of inline contents\",\"ExplanationImage\":\"link to the image that we show next to the given paragraph\"}],\"SetupInstructions\":[{\"InstructionType\":\"to be followed or terminal instruction\",\"InstructionStatement\":\"The statement of the instruction or the command to write in terminal\",\"InstructionReason\":\"this is the reason, or the ouput of performing the command\",\"InstructionImage\":\"there can be an instruction inage showning how to assemble the product\"},{\"InstructionType\":\"to be followed or terminal instruction\",\"InstructionStatement\":\"The statement of the instruction or the command to write in terminal\",\"InstructionReason\":\"this is the reason, or the ouput of performing the command\",\"InstructionImage\":\"there can be an instruction inage showning how to assemble the product\"}],\"RunInstruction\":[{\"InstructionType\":\"to be followed or terminal instruction\",\"InstructionStatement\":\"The statement of the instruction or the command to write in terminal\",\"InstructionReason\":\"this is the reason, or the ouput of performing the command\",\"InstructionImage\":\"there can be an instruction inage showning how to assemble the product\"},{\"InstructionType\":\"to be followed or terminal instruction\",\"InstructionStatement\":\"The statement of the instruction or the command to write in terminal\",\"InstructionReason\":\"this is the reason, or the ouput of performing the command\",\"InstructionImage\":\"there can be an instruction inage showning how to assemble the product\"}],\"UsageInstruction\":[{\"InstructionType\":\"to be followed or terminal instruction\",\"InstructionStatement\":\"The statement of the instruction or the command to write in terminal\",\"InstructionReason\":\"this is the reason, or the ouput of performing the command\",\"InstructionImage\":\"there can be an instruction inage showning how to assemble the product\"},{\"InstructionType\":\"to be followed or terminal instruction\",\"InstructionStatement\":\"The statement of the instruction or the command to write in terminal\",\"InstructionReason\":\"this is the reason, or the ouput of performing the command\",\"InstructionImage\":\"there can be an instruction inage showning how to assemble the product\"}]}");
