5cc30bd5a90074f47584d79e627352c448e3cbd7
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-07 23:02:58 -0700

    htp connection error enum not required

--- inc/http_connection_handler.h
+++ inc/http_connection_handler.h
@@ -3,16 +3,6 @@
 
 #include<stream.h>
 
-typedef enum http_connection_handler_error http_connection_handler_error;
-enum http_connection_handler_error
-{
-	REQUEST_PARSED_SUCCESSFULLY = 0,
-	NO_ERROR_REQUEST_NOT_PARSED_COMPLETELY_CONTINUE_READING = -1,
-	ERROR_OCCURRED_REQUEST_NOT_STANDARD_HTTP = -2,
-	TCP_CONNECTION_CLOSED_ABRUPTLY = -3,
-	TCP_CONNECTION_ERROR_READING = -4
-};
-
 void http_connection_stream_handler(stream* strm, void* server_specific_params);
 
 #endif
