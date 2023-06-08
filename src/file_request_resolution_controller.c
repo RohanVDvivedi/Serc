9e1eef992675caa32fc79c6c7099756803a4a146
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-08 05:02:26 -0700

    minor change

--- src/file_request_resolution_controller.c
+++ src/file_request_resolution_controller.c
@@ -2,35 +2,6 @@
 
 int file_request_controller(http_request_head* hrq, stream* strm, server_global_params* sgp, int* routing_resolved)
 {
-	if(hrq->method == GET && !is_empty_dstring(&(hrq->path)))
-	{
-		// extract extension dstring from the file path
-		dstring extension; init_empty_dstring(&extension, 0);
-		get_extension_from_file_path(&extension, &(hrq->path));
-
-		// read file contents in the response body dstring
-		if(read_file_in_dstring(&(hrp->body), server_file_cache, &(hrq->path)) == 0)
-    		*routing_resolved = 1;
-
-		// if the routing is resolved
-		if(*routing_resolved == 1)
-		{
-			// add content-type for the requested file, if there is extension on the file path
-			// and set tha status to 200
-			if(!is_empty_dstring(&extension))
-    			insert_unique_in_dmap_cstr(&(hrp->headers), "content-type", get_content_type_from_file_extension(&extension));
-
-    		hrp->status = 200;
-    	}
-
-    	// delete the extension and file_path dstrings
-    	deinit_dstring(&(extension));
-	}
-	return 0;
-}
-
-int file_request_controller(http_request_head* hrq, stream* strm, server_global_params* sgp, int* routing_resolved)
-{
 	// return value
 	int close_connection = 0;
 
