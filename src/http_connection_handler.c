22e3382d3c68dfa6d13dbd89b8b5d932d88386f8
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-08 05:23:01 -0700

    --

--- src/http_connection_handler.c
+++ src/http_connection_handler.c
@@ -14,17 +14,17 @@ void http_connection_stream_handler(stream* strm, void* server_specific_params)
 	while(1)
 	{
 		// create a new HttpRequest Object
-		http_request_head hrq; init_http_request(&hrq);
+		http_request_head hrq; init_http_request_head(&hrq);
 
 		if(-1 == parse_http_request_head(strm, &hrq))
 		{
-			deinit_http_request(&hrq);
+			deinit_http_request_head(&hrq);
 			break;
 		}
 
 		if(1 == distribute(&hrq, strm, sgpp))
 		{
-			deinit_http_request(&hrq);
+			deinit_http_request_head(&hrq);
 			break;
 		}
 
@@ -32,11 +32,11 @@ void http_connection_stream_handler(stream* strm, void* server_specific_params)
 		flush_all_from_stream(strm, &strm_flush_error);
 		if(0 != strm_flush_error)
 		{
-			deinit_http_request(&hrq);
+			deinit_http_request_head(&hrq);
 			break;
 		}
 
 		// deinitialize http_request Object
-		deinit_http_request(&hrq);
+		deinit_http_request_head(&hrq);
 	}
 }
