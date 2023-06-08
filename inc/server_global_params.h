fd33c0555906a84bf34c879b09bcdc447048f933
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-07 23:30:40 -0700

    removed non existant headers

--- inc/server_global_params.h
+++ inc/server_global_params.h
@@ -1,8 +1,6 @@
 #ifndef SERVER_GLOBAL_PARAMS_H
 #define SERVER_GLOBAL_PARAMS_H
 
-#include<file_handler.h>
-
 typedef struct server_global_params server_global_params;
 struct server_global_params
 {
