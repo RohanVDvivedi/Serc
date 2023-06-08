844bf5693d958c1b8486a1566e24448787165a63
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-07 23:45:17 -0700

    --

--- inc/http_server.h
+++ inc/http_server.h
@@ -7,7 +7,7 @@
 // else your network stack will be HTTP/TCP/IP
 void http_server_run(uint16_t PORT, char* ROOT_PATH, char* SSL_KEYS_CERTS);
 
-// if ROOT_PATH == NULL => the server would not serve andy static files
+// if ROOT_PATH == NULL => the server would not serve any static files
 // if SSL_KEYS_CERTS == NULL => the server would serve content on http, instead of https
 
 #endif
