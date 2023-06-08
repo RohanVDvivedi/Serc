86c31fdf611119bdcf2ce3c42122d0f8f64f121d
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-07 21:33:39 -0700

    removed redundant http parser code

--- inc/http_request.h
+++ inc/http_request.h
@@ -1,58 +1,7 @@
 #ifndef HTTP_REQUEST_H
 #define HTTP_REQUEST_H
 
-#include<dstring_hashmap.h>
-
-#include<http_method.h>
-#include<http_parse_context.h>
-
-#include<zlib_compression_wrapper.h>
-
-typedef struct HttpRequest HttpRequest;
-struct HttpRequest
-{
-	// if this response is being read and parsed
-	// then this is the context for it
-	HttpParseContext parseContext;
-
-	// the parsed contents of the response
-	HttpMethod method;
-	dstring path;
-	dstring version;
-	dmap parameters;
-	dmap headers;
-	dstring body;
-};
-
-// to init/create HttpRequest Object, by initializing and allocating memory for all its attributes
-void initHttpRequest(HttpRequest* hr);
-
-// parse string to populate HttpRequest
-int parseRequest(char* buffer, int buffer_size, HttpRequest* hr);
-
-// serialize HttpRequest and append to dstring to send over network or to print
-void serializeRequest(dstring* result, HttpRequest* hr);
-
 // set Default Header in Request like size , type , date , updated at , server type email etc
 void setServerDefaultHeadersInRequest(HttpRequest* hrq);
-
-// this will allow you to compress http response body, if it is not already compressed
-// this will also add appropriate content-type header to your http response
-void compressHttpRequestBody(HttpRequest* hrq, compression_type compr_type);
-
-// uncompress http request body
-void uncompressHttpRequestBody(HttpRequest* hrq);
-
-// release memory occupied by attributes of HttpResponse
-void deinitHttpRequest(HttpRequest* hr);
-
-// show on console a printable HttpRequest
-void printRequest(HttpRequest* hr);
-
-// returns the Cookie header if it is sent by the client
-dstring* getCookie(HttpRequest* hr);
-
-// serialize the url of the HttpRequest
-void serializeUrl(dstring* result, HttpRequest* hr);
 
 #endif
--- inc/http_response.h
+++ inc/http_response.h
@@ -1,50 +1,8 @@
 #ifndef HTTP_RESPONSE_H
 #define HTTP_RESPONSE_H
 
-#include<dstring_hashmap.h>
-
-#include<http_parse_context.h>
-
-#include<zlib_compression_wrapper.h>
-
-typedef struct HttpResponse HttpResponse;
-struct HttpResponse
-{
-	// if this response is being read and parsed
-	// then this is the context for it
-	HttpParseContext parseContext;
-
-	// the parsed contents of the response
-	dstring version;
-	int status;
-	dmap headers;
-	dstring body;
-};
-
-// to init/create HttpResponse Object, by initializing and allocating memory for all its attributes
-void initHttpResponse(HttpResponse* hr);
-
-// parse string to populate HttpResponse
-int parseResponse(char* buffer, int buffer_size, HttpResponse* hr);
-
-// serialize HttpResponse and append to dstring to send over network or to print
-void serializeResponse(dstring* result, HttpResponse* hr);
-
 // set Default Header in Request like size , type , date , updated at , server type email etc
 void setServerDefaultHeadersInResponse(HttpResponse* hrp);
-
-// this will allow you to compress http response body, if it is not already compressed
-// this will also add appropriate content-type header to your http response
-void compressHttpResponseBody(HttpResponse* hrp, compression_type compr_type);
-
-// uncompress HttpResponse Body
-void uncompressHttpResponseBody(HttpResponse* hrp);
-
-// release memory occupied by attributes of HttpResponse
-void deinitHttpResponse(HttpResponse* hr);
-
-// show on console a printable HttpResponse
-void printResponse(HttpResponse* hr);
 
 // sets a SetCookie header on the http response, this should be used to set a cookie on client
 // ehich the client will use while sending with every new request
