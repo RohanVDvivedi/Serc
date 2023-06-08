949b58d9a4bffe84c440da5a7d6ebc76df734682
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-07 23:28:58 -0700

    removed redundat code

--- inc/http_request.h
+++
@@ -1,7 +0,0 @@
-#ifndef HTTP_REQUEST_H
-#define HTTP_REQUEST_H
-
-// set Default Header in Request like size , type , date , updated at , server type email etc
-void setServerDefaultHeadersInRequest(HttpRequest* hrq);
-
-#endif
