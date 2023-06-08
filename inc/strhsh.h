3d51b1d39c3977746c15f0587be20f68b4c953f8
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-08 00:12:22 -0700

    definition change for strhsh

--- inc/strhsh.h
+++ inc/strhsh.h
@@ -8,6 +8,6 @@ unsigned long long int getHashValue(const char* s);
 
 unsigned long long int getHashValueDstring(const dstring* dstr);
 
-unsigned long long int getHashValueByLength(const char* s,int length);
+unsigned long long int getHashValueByLength(const char* s, unsigned long long int length);
 
 #endif
