0501a1f91a3ff85beb27986fc8a6aa3d82905933
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-10 04:03:50 -0700

    started implementing serve directories

--- inc/server_global_params.h
+++ inc/server_global_params.h
@@ -6,7 +6,11 @@
 typedef struct server_global_params server_global_params;
 struct server_global_params
 {
+	// root path of all the static files
 	dstring root_path;
+
+	// set this value if you want to serve directories at the root path
+	int serve_dirs;
 };
 
 #endif
